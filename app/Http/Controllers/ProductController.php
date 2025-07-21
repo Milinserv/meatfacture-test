87ce21b4f026b31ae5212c4fb178f0212287218d
Daniil <dhahalkin@tolkit.top>
2025-07-21 11:53:17 +0400

    removed unused imports from product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\Response;
-use App\Models\Product;
 use App\Services\ProductService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
