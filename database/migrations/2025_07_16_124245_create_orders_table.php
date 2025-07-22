ac16701ec06358f6e3b8c101270e015995c59d67
Daniil <dhahalkin@tolkit.top>
2025-07-22 10:50:32 +0400

    changed order migrate

--- database/migrations/2025_07_16_124245_create_orders_table.php
+++ database/migrations/2025_07_16_124245_create_orders_table.php
@@ -11,10 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->json('list_products');
+            $table->string('total_amount');
+            $table->text('comment');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('orders');
     }
 };
